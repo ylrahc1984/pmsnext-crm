77db148f196d23f8b60c4fcd512f24670ba5baa7
charly <coleccionista84@gmail.com>
2026-04-01 15:29:13 -0600

    Actualia url API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'https://api-crm.pmsnextdevcloud.com//api',
-  baseUrl: 'https://api-crm.pmsnextdevcloud.com//api',
+  apiUrl: 'https://api-crm.pmsnextdevcloud.com/api',
+  baseUrl: 'https://api-crm.pmsnextdevcloud.com/api',
   googleMapsApiKey: 'AIzaSyA-o3tTaSdRzt7JBBjRwBzYGNpUEtGyfF8'
 };
